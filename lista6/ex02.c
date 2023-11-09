d5236ac13452ce1ad66ff7d01d5a978dd47e3c07
Alex Sandro <alex.sandro.1199@gmail.com>
2023-11-09 14:42:46 -0300

    ex02 lista 5 corrigida

---
+++ lista6/ex02.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int i, avalia;
+    char str1[100], str2[100], str[10] = "alex", st[10] = "alex";
+
+    printf("Digite uma string: ");
+    gets(str1);
+    printf("Digite um string para comparacao: ");
+    gets(str2);
+
+    if (strlen(str1) == strlen(str2)) {
+        for (i = 0; i < strlen(str1); i++) {
+            if (str1[i] != str2[i]) {
+                avalia = 1;
+                break;
+            } else avalia = 0;
+        }
+    } else avalia = 1;
+
+    if (avalia == 1)
+        printf("As duas strings nao sao iguais!");
+    else
+        printf("As duas strings sao iguais!");
+
+    return 0;
+}
