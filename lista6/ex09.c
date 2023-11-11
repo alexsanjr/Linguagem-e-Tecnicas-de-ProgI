8bc0adb7b048e6857362fa9dfd672945ea590dc6
Alex Sandro <alex.sandro.1199@gmail.com>
2023-11-11 18:12:19 -0300

    ex09 completo

---
+++ lista6/ex09.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    int i, ascii;
+    char str[100];
+
+    printf("Digite uma frase em minusculo: ");
+    gets(str);
+
+    for (i = 0; i < strlen(str); i++) {
+        if (str[i] >= 97 && str[i] < 120)
+            str[i] += 3;
+        else if (str[i] >= 120 && str[i] <= 122)
+            str[i] = str[i] + 3 - 26;
+    }
+
+    for (i = 0; i < strlen(str); i++) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
+
