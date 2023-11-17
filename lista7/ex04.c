4b4ad478e2d1b3dd2144a107914d35de98f2702b
Alex Sandro <alex.sandro.1199@gmail.com>
2023-11-17 16:40:17 -0300

    ex04 lista 7 otimizado

--- lista7/ex04.c
+++ lista7/ex04.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
 
-#define TAM 4
+#define TAM 5
 
 int absoluto(int num) {
     if (num < 0 )
         return num *= -1;
     else
-        return  num;
+        return num;
 }
 
 int main() {
     int i, nums[TAM], abs[TAM];
 
-    for (i = 0; i <= TAM; ++i) {
-        printf("Digite um numero inteiro (%d/%d): ", i + 1, TAM + 1);
+    for (i = 0; i < TAM; ++i) {
+        printf("Digite um numero inteiro (%d/%d): ", i + 1, TAM);
         scanf("%d", &nums[i]);
     }
 
-    for (i = 0; i <= TAM; ++i)
+    for (i = 0; i < TAM; ++i)
         abs[i] = absoluto(nums[i]);
 
-
-    for (i = 0; i <= TAM; ++i) {
+    for (i = 0; i < TAM; ++i)
         printf("O valor absoluto de %d eh: %d\n", nums[i], abs[i]);
-    }
 
-
-
-
+    return 0;
 }
